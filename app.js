ce1763c569aaa26ff43352d22d2e4d9778a6fec5
katebarz <k.barzakovska@gmail.com>
2018-08-08 19:39:49 +0200

    Fixed schedule display

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//document.getElementById('content').style.backgroundImage = "url('cut.jpg')";
-
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 3000);
@@ -12,9 +10,7 @@ function nextSlide() {
 
 function dropdown() {
     if (window.innerWidth <= 767) {
-        /*alert(window.innerWidth);*/
         document.getElementById("dropdown-content").classList.toggle("show");
-        /*	document.getElementById("span-menu").style.transform="rotate(180deg)";*/
     }
 }
 window.onclick = function(event) {
@@ -28,108 +24,145 @@ window.onclick = function(event) {
         }
     }
 }
-/*if (document.getElementById("dropdown-content").contains("show")) {
-
-        document.getElementById("dropdown-content").remove('show');
-
-
-      }
-	}
-
-}*/
-/*const url ='https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014';
-var request = new XMLHttpRequest();
-request.open('GET',url);
-request.responseType='json';
-request.send();
-request.onload = function() {
-  var result = request.response;
-  console.log(result);
-  //populateHeader(superHeroes);
-  //showHeroes(superHeroes);
-  alert(result.bank);
-}*/
-
-
-/*var response =$.ajax({
-    url: 'https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014',
-    type: 'GET',
-    dataType: 'jsonp',
-    crossDomain: true,   
-});
-//var jsonObj = JSON.parse(response);
-console.log(response[0]);*/
-
-//var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
-//console.log(obj.name);
 
 fetch('https://statsapi.web.nhl.com/api/v1/standings')
     .then(function(response) {
-        (response.headers.get('Content-Type')); // application/json; charset=utf-8
+        // (response.headers.get('Content-Type')); // application/json; charset=utf-8
         //alert(response.status); // 200
         return response.json();
     })
     .then(function(result) {
-        var i;
-        var j;
-        /*for(j=0;j<4;j++){
-    	var newDiv = document.createElement('div');
-   		newDiv.id = 'col-'+j;
-   		newDiv.setAttribute('class', 'newDiv col-xs-12 col-sm-4 col-md-4 col-lg-2');
-   		document.getElementById('standings-col').appendChild(newDiv);
-    	var division = document.createElement('h3');
-    	division.innerHTML = (result.records[j]['division']['name']);
-    	document.getElementById(newDiv.id).appendChild(division);
-		    for(i=0;i<result.records[j]['teamRecords'].length;i++){
-		    	var teams = document.createElement('h4');
-		    	teams.innerHTML = (result.records[j]['teamRecords'][i]['team']['name']);
-		    	//div.setAttribute('class', 'myclass');
-				document.getElementById(newDiv.id).appendChild(teams);
-		    }
-	}
-	for(j=0;j<4;j++){
-    	var division = document.createElement('h3');
-    	division.innerHTML = (result.records[j]['division']['name']);
-    	document.getElementById("standings-col-2").appendChild(division);
-    	 for(i=0;i<result.records[j]['teamRecords'].length;i++){
-		    	var teams = document.createElement('div');
-		    	teams.innerHTML = (result.records[j]['teamRecords'][i]['team']['name']);
-		    	//teams.innerHTML = (result.records[j]['teamRecords'][i]['team']['link']);
-		    	//div.setAttribute('class', 'myclass');
-				document.getElementById("standings-col-2").appendChild(teams);
-		    }
-    }*/
-        /*var x,txt="";
-        txt += "<table border='1'>"
-        for (x in result) {
-            txt += "<tr><td>" + result[x].name + "</td></tr>";
-        }
-        txt += "</table>"        
-        document.getElementById("demo").innerHTML = txt;
-*/
         let records = result.records;
-        let table = "<table border='1'>"
+        let table = "<table>";
         for (record in records) {
             table += "<tr><th>" + records[record].division.name + "</th><th>Games Played</th><th>Wins</th><th>Losses</th><th>OT</th><th>Points</th>" +
                 "<th>Goals For</th><th>Goals Against</th><th>Conference Rank</th><th>League Rank</th><th>Division Rank</th></tr>";
             let teamRecords = result.records[record].teamRecords;
             for (teamRecord in teamRecords) {
-                table += "<tr><td>" + teamRecords[teamRecord].team.name + "</td>";
-                table += "<td>" + teamRecords[teamRecord].gamesPlayed + "</td><td>" + teamRecords[teamRecord].leagueRecord.wins + "</td><td>" +
-                    teamRecords[teamRecord].leagueRecord.losses + "</td><td>" + teamRecords[teamRecord].leagueRecord.ot + "</td><td>" +
-                    teamRecords[teamRecord].points + "</td><td>" + teamRecords[teamRecord].goalsScored + "</td><td>" +
-                    teamRecords[teamRecord].goalsAgainst + "</td><td>" +
-                    teamRecords[teamRecord].conferenceRank + "</td><td>" + teamRecords[teamRecord].leagueRank + "</td><td>" +
-                    teamRecords[teamRecord].divisionRank + "</td>";
-
-                table += "</tr>"
-                /*var teams = document.createElement('div');
-			    teams.innerHTML = teamRecords[teamRecord].team.name;
-				document.getElementById("standings-col-2").appendChild(teams);*/
+                table += "<tr><td><h4>" + teamRecords[teamRecord].team.name + "</h4></td>";
+                table += "<td><b>" + teamRecords[teamRecord].gamesPlayed + "</b></td><td><b>" + teamRecords[teamRecord].leagueRecord.wins + "</b></td><td><b>" +
+                    teamRecords[teamRecord].leagueRecord.losses + "</b></td><td><b>" + teamRecords[teamRecord].leagueRecord.ot + "</b></td><td><b>" +
+                    teamRecords[teamRecord].points + "</b></td><td><b>" + teamRecords[teamRecord].goalsScored + "</b></td><td><b>" +
+                    teamRecords[teamRecord].goalsAgainst + "</b></td><td><b>" +
+                    teamRecords[teamRecord].conferenceRank + "</b></td><td><b>" + teamRecords[teamRecord].leagueRank + "</b></td><td><b>" +
+                    teamRecords[teamRecord].divisionRank + "</b></td></tr>";
             }
-
+            table += "<tr></tr>";
         }
         table += "</table>";
         document.getElementById("standings-col").innerHTML = table;
-
     })
+
+////////////////////////// GET&PRINT SCHEDULE FROM API START /////////////////////////
+
+const getGamesFromApi = () => {
+
+    return fetch('https://api.sportradar.us/nhl/trial/v5/en/games/2018/REG/schedule.json?api_key=jjxpjasm445fcjwt6cdhtgdw')
+        .then(res => { return res.json(); })
+        .then(game => {
+            let games = game.games
+            let gamesToPrint = []
+            for (game in games) {
+                if ((games[game].home.alias == "SJ" || games[game].away.alias == "SJ")) {
+                    gamesToPrint.push(games[game])
+                }
+            }
+            return (gamesToPrint)
+        })
+}
+
+const games = getGamesFromApi()
+
+let indexX = 0
+let indexY = 0
+
+let showNextBtn = document.getElementById("showNext");
+showNextBtn.onclick = () => {
+    //add if > games.length 
+    indexX = indexY
+    indexY = indexX + 7
+    printGames(indexX, indexY)
+}
+
+let showPrevBtn = document.getElementById("showPrev");
+showPrevBtn.onclick = () => {
+    //add if < 0
+    indexY = indexX
+    indexX = indexY - 7
+    printGames(indexX, indexY)
+}
+printGames = (indexX, indexY) => {
+    let table = "<table>";
+    games.then((data) => {
+        let games = data.slice(indexX, indexY)
+        for (game in games) {
+            let time = new Date(games[game].scheduled).getMinutes().toString();
+            (time.length == 1) ? time += '0': time;
+            table += "<tr><td><b>" + new Date(games[game].scheduled).toString().substr(0, 10).replace(/\b0/g, '') + "</b></td><td><b>" + games[game].home.name + "</b></td><td><b>" +
+                games[game].away.name + "</b></td><td><b>" + new Date(games[game].scheduled).getHours() + ":" + time + "</b></td><td><b>" +
+                games[game].venue.city + "," + games[game].venue.name + "</b></td></tr>";
+        }
+        document.getElementById("schedule-col").innerHTML = table;
+    })
+}
+
+showNextBtn.click() // to display first 10 elems
+
+
+
+////////////////////////// GET&PRINT SCHEDULE FROM API END /////////////////////////
+
+//// old version
+
+/*const games=fetch('https://api.sportradar.us/nhl/trial/v5/en/games/2018/REG/schedule.json?api_key=jjxpjasm445fcjwt6cdhtgdw')
+    .then(function(response) {
+        //alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
+        //alert(response.status); // 200
+
+        return response.json();
+    })
+    
+      .then(function(game) {
+            document.getElementById("schedule-col").innerHTML=game.games[0].id;
+            let table = "<table>";       
+            let games = game.games;
+            for(game in games){
+                if((games[game].home.alias=="SJ"||games[game].away.alias=="SJ")){
+                    let time=new Date(games[game].scheduled).getMinutes().toString();
+                    (time.length==1)?time+='0':time;
+                table+="<tr><td><b>"+new Date(games[game].scheduled).toString().substr(0,10).replace(/\b0/g, '')+"</b></td><td><b>"+games[game].home.name+"</b></td><td><b>"+
+                games[game].away.name+"</b></td><td><b>"+new Date(games[game].scheduled).getHours()+":"+time+"</b></td><td><b>"+
+                games[game].venue.city + ","+ games[game].venue.name+"</b></td></tr>";}
+            }
+
+            document.getElementById("schedule-col").innerHTML = table;
+      })
+
+        .then(function(game) {
+        let games = game.games;
+        let gamesToPrint = [];
+        for (game in games) {
+            if ((games[game].home.alias == "SJ" || games[game].away.alias == "SJ")) {
+                gamesToPrint.push(games[game]);
+            }
+        }
+        console.log(gamesToPrint);
+        return(gamesToPrint);
+    })
+
+*/
+/*function printGames(games) {
+    let table = "<table>";
+    var x=0,y=10;
+    console.log(games);
+    for (game in games.slice(x,y)) {
+        {
+            let time = new Date(games[game].scheduled).getMinutes().toString();
+            (time.length == 1) ? time += '0': time;
+            table += "<tr><td><b>" + new Date(games[game].scheduled).toString().substr(0, 10).replace(/\b0/g, '') + "</b></td><td><b>" + games[game].home.name + "</b></td><td><b>" +
+                games[game].away.name + "</b></td><td><b>" + new Date(games[game].scheduled).getHours() + ":" + time + "</b></td><td><b>" +
+                games[game].venue.city + "," + games[game].venue.name + "</b></td></tr>";
+        }
+        document.getElementById("schedule-col").innerHTML = table;
+    }
+}*/
