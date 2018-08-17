39617f6785e2be52e0c636d1dd56e25d7c74e244
katebarz <k.barzakovska@gmail.com>
2018-08-17 12:27:25 +0200

    Scroll, onload,hightlightning, footer

--- app.js
+++ app.js
@@ -1,17 +1,88 @@
 ///////////////////////////Active Menu Highlight/////////////////////
-let navDiv = document.getElementById("navbar-main");
+/*let navDiv = document.getElementById("navbar-main");
 let links = navDiv.getElementsByClassName("nav-link");
 for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active-li");
-    current[0].className = current[0].className.replace(" active-li", "");
-    this.className += " active-li";
-  });
+    links[i].addEventListener("click", function() {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        console.log(top)
+        $('body,html').animate({scrollTop: top}, 1500);
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
+*/
+// $(function(){
+
+/*    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });*/
+$(window).on("load", () => {
+  /*$("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } 
+  });*/
+  window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+    $('.navbar-nav>li:nth-child(1)').addClass('activeLink');
+    var topOffsetTop = $('.slider').offset().top;
+    var teamOffsetTop = $('.team').offset().top;
+    var scheduleOffsetTop = $('.schedule').offset().top;
+    var standingOffsetTop = $('.standings').offset().top;
+    var contactOffsetTop = $('.contact').offset().top;
+    $(document).on('scroll', function() {
+        var scrollTop = (window.pageYOffset)+120;
+        var activeLi;
+        if (scrollTop < teamOffsetTop) {
+            activeLi = $('.navbar-nav>li:nth-child(1)');
+        } else if (scrollTop > teamOffsetTop && scrollTop < scheduleOffsetTop) {
+            activeLi = $('.navbar-nav>li:nth-child(2)');
+        } else if (scrollTop > scheduleOffsetTop && scrollTop < standingOffsetTop) {
+            activeLi = $('.navbar-nav>li:nth-child(3)');
+        } else if (scrollTop > standingOffsetTop && scrollTop < contactOffsetTop-350) {
+            activeLi = $('.navbar-nav>li:nth-child(4)');
+        } else if (scrollTop > contactOffsetTop-350) {
+            activeLi = $('.navbar-nav>li:nth-child(5)');
+        }
+        activeLi.addClass(' activeLink')
+        $('.navbar-nav>li').not(activeLi).removeClass(' activeLink');
+    });
+
+});
 //////////////////////SLIDES////////////////////////////////////
+
 let slides = document.querySelectorAll('#slides .slide');
 let currentSlide = 0;
 let slideInterval = setInterval(nextSlide, 3000);
+
 function nextSlide() {
     slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide + 1) % slides.length;
@@ -36,12 +107,14 @@ window.onclick = function(event) {
 }
 
 ////////////////////////// GET&PRINT SCHEDULE FROM API START /////////////////////////
-const proxyUrl="https://cors-anywhere.herokuapp.com/";
-const url = "https://api.sportradar.us/nhl/trial/v5/en/games/2018/REG/schedule.json?api_key=jjxpjasm445fcjwt6cdhtgdw";
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://api.sportradar.us/nhl/trial/v5/en/games/2018/REG/schedule.json?api_key=4s6f2qtmhztsfngr6kn4624w";
+document.getElementById("schedule-col").innerHTML = "<img src=\"images\\Loading_icon.gif\" class=\"schedule-load\">";
 const getGamesFromApi = () => {
-
-    return fetch(proxyUrl+url)
-        .then(res => { return res.json(); })
+    return fetch(proxyUrl + url)
+        .then(res => {
+            return res.json();
+        })
         .then(game => {
             let games = game.games
             let gamesToPrint = []
@@ -60,10 +133,10 @@ let indexX = 0
 let indexY = 0
 let showNextBtn = document.getElementById("showNext");
 showNextBtn.onclick = () => {
-    if(indexY<82){
-    //add if > games.length 
-    indexX = indexY
-    indexY = indexX + 7
+    if (indexY < 82) {
+        //add if > games.length 
+        indexX = indexY
+        indexY = indexX + 7
     }
     printGames(indexX, indexY)
 }
@@ -71,9 +144,9 @@ showNextBtn.onclick = () => {
 let showPrevBtn = document.getElementById("showPrev");
 showPrevBtn.onclick = () => {
     //add if < 0
-    if(indexX>0){
-    indexY = indexX
-    indexX = indexY - 7
+    if (indexX > 0) {
+        indexY = indexX
+        indexX = indexY - 7
     }
     printGames(indexX, indexY)
 }
@@ -99,65 +172,65 @@ showNextBtn.click() // to display first 10 elems
 ////////////////////////// GET&PRINT STANDINGS FROM API START /////////////////////////
 const getStandingsFromApi = () => {
     return fetch('https://statsapi.web.nhl.com/api/v1/standings')
-    .then(result => {return result.json();})
-    .then(function(result) {
-        let records = result.records;
-        let standing = []
-        for(record in records)
-            standing.push(records[record])
-        return standing;
-    })
+        .then(result => { return result.json(); })
+        .then(function(result) {
+            let records = result.records;
+            let standing = []
+            for (record in records)
+                standing.push(records[record])
+            return standing;
+        })
 }
 const standings = getStandingsFromApi();
 let showMetropolitan = document.getElementById("metropolitan");
-showMetropolitan.onclick = () =>{
+showMetropolitan.onclick = () => {
     printStandings(0);
 }
 
 let showAtlantic = document.getElementById("atlantic");
-showAtlantic.onclick = () =>{
+showAtlantic.onclick = () => {
     printStandings(1);
 }
 
 let showCentral = document.getElementById("central");
-showCentral.onclick = () =>{
+showCentral.onclick = () => {
     printStandings(2);
 }
 
 let showPacific = document.getElementById("pacific");
-showPacific.onclick = () =>{
+showPacific.onclick = () => {
     printStandings(3);
 }
 var btnsDiv = document.getElementById("standings-btn");
 var btns = btnsDiv.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active-division");
-    current[0].className = current[0].className.replace(" active-division", "");
-    this.className += " active-division";
-  });
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active-division");
+        current[0].className = current[0].className.replace(" active-division", "");
+        this.className += " active-division";
+    });
 }
 
 printStandings = (indexA) => {
     let table = "<table>";
     standings.then((data) => {
         let records = data[indexA]
-            table += "<tr><th>" +  records.division.name + "</th><th>Games Played</th><th>Wins</th><th>Losses</th><th>OT</th><th>Points</th>" +
-                "<th>Goals For</th><th>Goals Against</th><th>Conference Rank</th><th>League Rank</th><th>Division Rank</th></tr>";
-            let teamRecords =  records.teamRecords;
-            for (teamRecord in teamRecords) {
-                table += "<tr><td><h4>" + teamRecords[teamRecord].team.name + "</h4></td>";
-                table += "<td><b>" + teamRecords[teamRecord].gamesPlayed + "</b></td><td><b>" + teamRecords[teamRecord].leagueRecord.wins + "</b></td><td><b>" +
-                    teamRecords[teamRecord].leagueRecord.losses + "</b></td><td><b>" + teamRecords[teamRecord].leagueRecord.ot + "</b></td><td><b>" +
-                    teamRecords[teamRecord].points + "</b></td><td><b>" + teamRecords[teamRecord].goalsScored + "</b></td><td><b>" +
-                    teamRecords[teamRecord].goalsAgainst + "</b></td><td><b>" +
-                    teamRecords[teamRecord].conferenceRank + "</b></td><td><b>" + teamRecords[teamRecord].leagueRank + "</b></td><td><b>" +
-                    teamRecords[teamRecord].divisionRank + "</b></td></tr>";
-            }
-            table += "<tr></tr>";
+        table += "<tr><th>" + records.division.name + "</th><th>Games Played</th><th>Wins</th><th>Losses</th><th>OT</th><th>Points</th>" +
+            "<th>Goals For</th><th>Goals Against</th><th>Conference Rank</th><th>League Rank</th><th>Division Rank</th></tr>";
+        let teamRecords = records.teamRecords;
+        for (teamRecord in teamRecords) {
+            table += "<tr><td><h4>" + teamRecords[teamRecord].team.name + "</h4></td>";
+            table += "<td><b>" + teamRecords[teamRecord].gamesPlayed + "</b></td><td><b>" + teamRecords[teamRecord].leagueRecord.wins + "</b></td><td><b>" +
+                teamRecords[teamRecord].leagueRecord.losses + "</b></td><td><b>" + teamRecords[teamRecord].leagueRecord.ot + "</b></td><td><b>" +
+                teamRecords[teamRecord].points + "</b></td><td><b>" + teamRecords[teamRecord].goalsScored + "</b></td><td><b>" +
+                teamRecords[teamRecord].goalsAgainst + "</b></td><td><b>" +
+                teamRecords[teamRecord].conferenceRank + "</b></td><td><b>" + teamRecords[teamRecord].leagueRank + "</b></td><td><b>" +
+                teamRecords[teamRecord].divisionRank + "</b></td></tr>";
+        }
+        table += "<tr></tr>";
         table += "</table>";
-                document.getElementById("standings-col").innerHTML = table;
-        })
+        document.getElementById("standings-col").innerHTML = table;
+    })
 }
 showMetropolitan.click()
 
